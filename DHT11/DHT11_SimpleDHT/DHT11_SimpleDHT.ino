beaf3eec967978a15779774d45aa12d0824ef0be
Adrian Hum <adrian@littlegreenmartian.net>
2018-07-08 19:58:58 +1000

    new snippets

---
+++ DHT11/DHT11_SimpleDHT/DHT11_SimpleDHT.ino
@@ -0,0 +1,100 @@
+#include <ESP8266WiFi.h>
+#include <SimpleDHT.h>
+#include <PubSubClient.h>
+
+// for DHT11,
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 14;
+SimpleDHT22 dht11;
+
+const char* wlan_ssid   =  "TelstraAF73E6";
+const char* wlan_pass   = "mxcv6kfa9dbc";
+String mqtt_topic   = "feeds/temphumid";
+const char* mqtt_server = "10.0.0.1";
+/************************* Adafruit.io Setup *********************************/
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+void setup() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wlan_ssid);
+
+  WiFi.begin(wlan_ssid, wlan_ssid);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("feeds/temphumid", "started");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err); delay(1000);
+    return;
+  }
+
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  Serial.print((int)temperature); Serial.print(" *C, ");
+  Serial.print((int)humidity); Serial.println(" H");
+  char data[100];
+  snprintf(data,sizeof(data), "{\"temp\":%04d,\"humid\":%04d}", (int)temperature, (int)humidity);
+  client.publish("feeds/temphumid", data);
+  // DHT11 sampling rate is 1HZ.
+  delay(2500);
+}
+
+
---
+++ DHT11/DHT11_SimpleDHT2/DHT11_SimpleDHT2.ino
@@ -0,0 +1,93 @@
+#include <SimpleDHT.h>
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define WLAN_SSID       "TelstraAF73E6"  // can't be longer than 32 characters!
+#define WLAN_PASS       "mxcv6kfa9dbc"
+#define AIO_SERVER      "10.0.0.1"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "Adrian"
+#define AIO_KEY         "Key"
+
+// for DHT11,
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT = 14;
+SimpleDHT22 dht;
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Publish temphumid = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temphumid");
+
+void setup() {
+Serial.begin(115200);
+  delay(10);
+ 
+  Serial.println(F("MQTT demo"));
+ 
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+ 
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+ 
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+ MQTT_connect();
+
+
+  
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht.read(pinDHT, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT failed, err="); Serial.println(err); delay(1000);
+    return;
+  }
+
+  Serial.print((int)temperature); Serial.print(" *C, ");
+  Serial.print((int)humidity); Serial.println(" H");
+  char data[100];
+  snprintf(data,sizeof(data), "{\"temp\":%04d,\"humid\":%04d}", (int)temperature, (int)humidity);
+  Serial.print(data);
+ temphumid.publish(data);
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(1500);
+}
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
+
